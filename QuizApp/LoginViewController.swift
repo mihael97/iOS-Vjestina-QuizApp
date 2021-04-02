369f94ea8271beefa8ff0766a23a7a8dbc75ea22
zwickzange <54530081+zwickzange@users.noreply.github.com>
2021-04-02 16:54:37 +0200

    Username field

--- QuizApp/LoginViewController.swift
+++ QuizApp/LoginViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
     let fontName = "ArialRoundedMTBold"
-    
+
     private var appNameLabel: UILabel!
+    private var usernameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,21 +27,36 @@ class LoginViewController: UIViewController {
     
     private func styleView() {
         appNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             appNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             appNameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            usernameTextField.widthAnchor.constraint(equalToConstant: 300),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 40),
+            usernameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            usernameTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
     
     private func createViewComponents() {
         view.backgroundColor = .purple
 
+        // Quiz app Styling
         appNameLabel = UILabel()
         appNameLabel.text = "Quiz App"
         appNameLabel.font = UIFont(name:fontName, size: 50.0)
         appNameLabel.textColor = .systemYellow
+        
+        // Username field Styling
+        usernameTextField = UITextField()
+        usernameTextField.placeholder = "Email"
+        usernameTextField.layer.cornerRadius = 5
+        usernameTextField.backgroundColor = .systemPurple
+        usernameTextField.textColor = .black
 
+        //Add layouts
         view.addSubview(appNameLabel)
+        view.addSubview(usernameTextField)
     }
     
 }
