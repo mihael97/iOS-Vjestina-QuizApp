33f5ccc5b2a1be8eff4dbeeeb0ac98cae2487a85
zwickzange <54530081+zwickzange@users.noreply.github.com>
2021-04-05 14:46:01 +0200

    Invalid credentials label

--- QuizApp/LoginViewController.swift
+++ QuizApp/LoginViewController.swift
@@ -18,6 +18,8 @@ class LoginViewController: UIViewController {
     private var passwordField: UITextField!
     private var loginButton: UIButton!
     private var dataService: DataService!
+    private var isFalseLogin = true
+    private var falseLoginLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +37,7 @@ class LoginViewController: UIViewController {
         usernameTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordField.translatesAutoresizingMaskIntoConstraints = false
         loginButton.translatesAutoresizingMaskIntoConstraints = false
+        falseLoginLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             appNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             appNameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
@@ -50,6 +53,31 @@ class LoginViewController: UIViewController {
             loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             loginButton.widthAnchor.constraint(equalToConstant: 100)
         ])
+        if(!falseLoginLabel.isHidden) {
+            NSLayoutConstraint.activate([
+                falseLoginLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                falseLoginLabel.bottomAnchor.constraint(equalTo:usernameTextField.topAnchor ,constant: -10),
+                falseLoginLabel.widthAnchor.constraint(equalToConstant:  200)
+            ])
+        }
+
+    }
+    
+    private func addTrailingBulb() {
+        let largeFont = UIFont.systemFont(ofSize: 30)
+        let configuration = UIImage.SymbolConfiguration(font: largeFont)
+
+        let image = UIImage(systemName: "lightbulb.fill", withConfiguration: configuration)
+        
+        let bulbAttachment = NSTextAttachment()
+        bulbAttachment.image = image
+        let attachmentString = NSAttributedString(attachment:  bulbAttachment)
+        let string = NSMutableAttributedString(string: "", attributes: [:])
+        if let labelValue = passwordField.text {
+            string.append(NSMutableAttributedString(string: String(format:" %@",labelValue), attributes: [:]))
+        }
+        string.append(attachmentString)
+        passwordField.attributedText = string
     }
     
     private func createViewComponents() {
@@ -67,13 +95,19 @@ class LoginViewController: UIViewController {
         usernameTextField.layer.cornerRadius = CGFloat(radiusOfField)
         usernameTextField.backgroundColor = .systemPurple
         usernameTextField.textColor = .black
+        usernameTextField.addTarget(self, action: #selector(LoginViewController.textFieldDidChange(_:)), for: .editingChanged)
         
         // Password field Styling
         passwordField = UITextField()
         passwordField.placeholder = "Password"
         passwordField.layer.cornerRadius = CGFloat(radiusOfField)
         passwordField.backgroundColor = .systemPurple
-        usernameTextField.textColor = .black
+        passwordField.textColor = .black
+        passwordField.isSecureTextEntry = true
+        passwordField.addTarget(self, action: #selector(LoginViewController.textFieldDidChange(_:)), for: .editingChanged)
+
+        
+        addTrailingBulb()
         
         // Login button
         loginButton = UIButton()
@@ -81,22 +115,38 @@ class LoginViewController: UIViewController {
         loginButton.backgroundColor = .blue
         loginButton.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
         
-        
+        // False login label
+        falseLoginLabel = UILabel()
+        falseLoginLabel.textColor = .white
+        falseLoginLabel.backgroundColor = .systemRed
+        falseLoginLabel.text = "Wrong credentials"
+        falseLoginLabel.textAlignment = .center
+        falseLoginLabel.isHidden = true
+    
         // Add layouts
         view.addSubview(appNameLabel)
         view.addSubview(usernameTextField)
         view.addSubview(passwordField)
         view.addSubview(loginButton)
+        view.addSubview(falseLoginLabel)
     }
     
    @objc func pressed(sender: UIButton!) {
     let response :LoginStatus = dataService.login(email: usernameTextField.text ?? "", password: passwordField.text ?? "")
     switch response {
     case .error(_,_):
-        print("Failed failed with error")
+        print("Loggin failed with error")
+        falseLoginLabel.isHidden = false
+        styleView()
         break
     case .success:
         print("Loggin is successfull")
     }
    }
+    
+    @objc func textFieldDidChange(_:UITextField)  {
+        if(isFalseLogin) {
+            falseLoginLabel.isHidden = true
+        }
+    }
 }
