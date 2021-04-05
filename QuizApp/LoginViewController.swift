01e3acc38a91edf34a3cf3c3ad42faaf60ae2326
zwickzange <54530081+zwickzange@users.noreply.github.com>
2021-04-05 19:15:42 +0200

    Button for password visibility

--- QuizApp/LoginViewController.swift
+++ QuizApp/LoginViewController.swift
@@ -10,12 +10,12 @@ import Foundation
 import UIKit
 
 class LoginViewController: UIViewController {
-    let fontName = "ArialRoundedMTBold"
-    let radiusOfField = 5
+    private let fontName: String = "ArialRoundedMTBold"
+    private let radiusOfField:Int64 = 5
 
     private var appNameLabel: UILabel!
     private var usernameTextField: UITextField!
-    private var passwordField: UITextField!
+    private var passwordField: PasswordField!
     private var loginButton: UIButton!
     private var dataService: DataService!
     private var isFalseLogin = true
@@ -38,9 +38,13 @@ class LoginViewController: UIViewController {
         passwordField.translatesAutoresizingMaskIntoConstraints = false
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         falseLoginLabel.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
             appNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             appNameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            falseLoginLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            falseLoginLabel.bottomAnchor.constraint(equalTo:usernameTextField.topAnchor ,constant: -10),
+            falseLoginLabel.widthAnchor.constraint(equalToConstant:  200),
             usernameTextField.widthAnchor.constraint(equalToConstant: 300),
             usernameTextField.heightAnchor.constraint(equalToConstant: 40),
             usernameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -53,31 +57,6 @@ class LoginViewController: UIViewController {
             loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             loginButton.widthAnchor.constraint(equalToConstant: 100)
         ])
-        if(!falseLoginLabel.isHidden) {
-            NSLayoutConstraint.activate([
-                falseLoginLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                falseLoginLabel.bottomAnchor.constraint(equalTo:usernameTextField.topAnchor ,constant: -10),
-                falseLoginLabel.widthAnchor.constraint(equalToConstant:  200)
-            ])
-        }
-
-    }
-    
-    private func addTrailingBulb() {
-        let largeFont = UIFont.systemFont(ofSize: 30)
-        let configuration = UIImage.SymbolConfiguration(font: largeFont)
-
-        let image = UIImage(systemName: "lightbulb.fill", withConfiguration: configuration)
-        
-        let bulbAttachment = NSTextAttachment()
-        bulbAttachment.image = image
-        let attachmentString = NSAttributedString(attachment:  bulbAttachment)
-        let string = NSMutableAttributedString(string: "", attributes: [:])
-        if let labelValue = passwordField.text {
-            string.append(NSMutableAttributedString(string: String(format:" %@",labelValue), attributes: [:]))
-        }
-        string.append(attachmentString)
-        passwordField.attributedText = string
     }
     
     private func createViewComponents() {
@@ -95,25 +74,20 @@ class LoginViewController: UIViewController {
         usernameTextField.layer.cornerRadius = CGFloat(radiusOfField)
         usernameTextField.backgroundColor = .systemPurple
         usernameTextField.textColor = .black
-        usernameTextField.addTarget(self, action: #selector(LoginViewController.textFieldDidChange(_:)), for: .editingChanged)
+        usernameTextField.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 0);
+        addDidChangeTrigger(element: usernameTextField)
         
         // Password field Styling
-        passwordField = UITextField()
-        passwordField.placeholder = "Password"
-        passwordField.layer.cornerRadius = CGFloat(radiusOfField)
+        passwordField = PasswordField()
         passwordField.backgroundColor = .systemPurple
         passwordField.textColor = .black
-        passwordField.isSecureTextEntry = true
-        passwordField.addTarget(self, action: #selector(LoginViewController.textFieldDidChange(_:)), for: .editingChanged)
-
-        
-        addTrailingBulb()
+        addDidChangeTrigger(element: passwordField)
         
         // Login button
         loginButton = UIButton()
         loginButton.setTitle("Login", for: .normal)
         loginButton.backgroundColor = .blue
-        loginButton.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
+        loginButton.addTarget(self, action: #selector(self.login), for: .touchUpInside)
         
         // False login label
         falseLoginLabel = UILabel()
@@ -131,22 +105,28 @@ class LoginViewController: UIViewController {
         view.addSubview(falseLoginLabel)
     }
     
-   @objc func pressed(sender: UIButton!) {
-    let response :LoginStatus = dataService.login(email: usernameTextField.text ?? "", password: passwordField.text ?? "")
-    switch response {
-    case .error(_,let message):
-        print("Loggin failed with error: \(message)")
-        falseLoginLabel.isHidden = false
-        styleView()
-        break
-    case .success:
-        print("Loggin is successfull")
+    private func addDidChangeTrigger(element: UITextField) {
+        element.addTarget(self, action: #selector(LoginViewController.textFieldDidChange(_:)), for: .editingChanged)
     }
-   }
     
-    @objc func textFieldDidChange(_:UITextField)  {
+    @objc
+    func login(sender: UIButton!) {
+        let response :LoginStatus = dataService.login(email: usernameTextField.text ?? "", password: passwordField.text ?? "")
+        switch response {
+            case .error(_,let message):
+                print("Loggin failed with error: \(message)")
+                falseLoginLabel.isHidden = false
+                break
+            case .success:
+                print("Loggin is successful")
+        }
+    }
+    
+    @objc
+    func textFieldDidChange(_:UITextField)  {
         if(isFalseLogin) {
             falseLoginLabel.isHidden = true
         }
     }
+
 }
---
+++ QuizApp/components/PasswordField.swift
@@ -0,0 +1,55 @@
+//
+//  PasswordField.swift
+//  QuizApp
+//
+//  Created by five on 05/04/2021.
+//  Copyright © 2021 five. All rights reserved.
+//
+
+import SwiftUI
+
+class PasswordField: UITextField {
+    private let radiusOfField:Int64 = 5
+    private let offsetOfField:Int64 = 5
+    private let openEyeImageName: UIImage? = UIImage(systemName: "eye")
+    private let closeEyeImageName: UIImage? = UIImage(systemName: "eye.slash")
+    private var passwordShowButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("A")
+        setupField()
+        addVisiblityButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupField() {
+        self.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 0);
+        self.isSecureTextEntry = true
+        self.placeholder = "Password"
+    }
+        
+    private func addVisiblityButton() {
+        passwordShowButton = UIButton()
+        passwordShowButton.setImage(openEyeImageName, for: .normal)
+        passwordShowButton.imageEdgeInsets = UIEdgeInsets(top: 5, left: -24, bottom: 5, right: 15)
+        passwordShowButton.frame = CGRect(x: CGFloat(self.frame.size.width - 25), y: CGFloat(5), width: CGFloat(15), height: CGFloat(25))
+        passwordShowButton.addTarget(self, action: #selector(self.visibilityButtonClicked), for: .touchUpInside)
+        self.rightView = passwordShowButton
+        self.rightViewMode = .always
+    }
+        
+    @objc
+    private func visibilityButtonClicked(sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        if sender.isSelected {
+            passwordShowButton.setImage(closeEyeImageName, for: .normal)
+        } else {
+            passwordShowButton.setImage(openEyeImageName, for: .normal)
+        }
+        self.isSecureTextEntry = !self.isSecureTextEntry
+    }
+}
