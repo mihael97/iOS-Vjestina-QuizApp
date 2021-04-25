fd2498bdbe6b7c68f77797f23a7334af550847d1
Mihael Macuka <mihaelmacuka2@gmail.com>
2021-04-25 18:39:42 +0200

    Progress view

---
+++ QuizApp/components/quiz/question/QuizQuestionResponse.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestionResponse.swift
+//  QuizApp
+//
+//  Created by five on 25/04/2021.
+//  Copyright © 2021 five. All rights reserved.
+//
+
+import Foundation
+enum QuizQuestionResponse {
+    case CORRECT
+    case INCORRECT
+    case EMPTY
+}
