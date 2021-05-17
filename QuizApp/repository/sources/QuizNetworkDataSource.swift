668ed25daa62868ebc28e57eaba2b4a17c57661d
Mihael Macuka <mihaelmacuka2@gmail.com>
2021-05-17 14:41:18 +0200

    Data source architecture

---
+++ QuizApp/repository/sources/QuizNetworkDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  QuizNetworkDataSource.swift
+//  QuizApp
+//
+//  Created by five on 17/05/2021.
+//  Copyright © 2021 five. All rights reserved.
+//
+
+import Foundation
+
+class QuizNetworkDataSource {
+    
+}
