4e66d6aa13d978fc7b30d134275f0450b1684cdb
Mihael Macuka <mihaelmacuka2@gmail.com>
2021-05-26 19:47:39 +0200

    Search

--- QuizApp/view/SearchQuizViewController.swift
+++ QuizApp/view/SearchQuizViewController.swift
@@ -13,11 +13,15 @@ class SearchQuizViewController: UIViewController {
     private var router: AppRouterProtocol!
     private var networkManager: NetworkServiceProtocol!
     private var searchBar: SearchBarView!
+    private var quizCollection: QuizCollection!
+    private var presenter: SearchBarViewPresenter!
     
     convenience init (router: AppRouterProtocol, networkManager: NetworkServiceProtocol) {
         self.init()
         self.router = router
         self.networkManager = networkManager
+        self.presenter = SearchBarViewPresenter(networkManager: networkManager)
+        self.presenter.setDelegate(delegate: self)
     }
         
     override func viewDidLoad() {
@@ -28,9 +32,13 @@ class SearchQuizViewController: UIViewController {
     
     private func buildView() {
         self.view.backgroundColor = .purple
-        searchBar = SearchBarView()
+        searchBar = SearchBarView(presenter: self.presenter)
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(searchBar)
+        
+        quizCollection = QuizCollection(router: router)
+        quizCollection.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(quizCollection)
     }
     
     private func setConstraints() {
@@ -40,6 +48,14 @@ class SearchQuizViewController: UIViewController {
             searchBar.heightAnchor.constraint(equalToConstant: 30),
             searchBar.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
             searchBar.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 10),
+            quizCollection.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            quizCollection.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10)
         ])
+    }
+}
+
+extension SearchQuizViewController: SearchBarViewDelegate {
+    func searchResults(quizzes: [QuizCategory : [Quiz]]) {
+        quizCollection.update(quizzes: quizzes)
     }
 }
